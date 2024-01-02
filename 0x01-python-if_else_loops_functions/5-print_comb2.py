024197983504dd8858f4d460e3faeff4730fb8db
Tunde <tundeogunmodede1@gmail.com>
2024-01-02 15:12:26 -0800

    5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 for i in range(100):
-    if 1 < 99:
-        print("{:02d}, ".format(i), end="")
-    else:
-        print("{:02d}".format(i), end="\n")
+        print("{:02d},".format(i), end=" ")
+print('99')
