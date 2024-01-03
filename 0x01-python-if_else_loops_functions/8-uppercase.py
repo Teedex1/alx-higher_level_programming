88c752bfc387e042b6a77818009e151b38abdcee
Tunde <tundeogunmodede1@gmail.com>
2024-01-02 17:04:49 -0800

    8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,5 +3,5 @@ def uppercase(input_str):
     for char in input_str:
         if 'a' <= char <= 'z':
             char = chr(ord(char) - ord('a') + ord('A'))
-            print(char, end="")
+            print("{}".format(char), end="")
     print()
