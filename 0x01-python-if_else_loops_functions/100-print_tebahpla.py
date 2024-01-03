9ac41b2754b6cc078202f4b5e2eb853bebe46a32
Tunde <tundeogunmodede1@gmail.com>
2024-01-03 08:22:05 -0800

    100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for i in range(ord('z'), ord('A'), -2):
-    print("{}".format(chr(i) + chr(i - 33)), end="")
+for i in range(ord('z'), ord('A') - 1, -2):
+    if i >= ord('a'):
+        print("{}".format(chr(i) + chr(i - 33)), end="")
 print()
