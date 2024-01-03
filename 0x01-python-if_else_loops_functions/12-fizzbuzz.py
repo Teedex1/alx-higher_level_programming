172b411ba13edcc849d0262919b6dfb5eab13fa4
Tunde <tundeogunmodede1@gmail.com>
2024-01-02 18:31:18 -0800

    12-fizzbuzz.py

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def fizzbuzz():
+    for number in range(1, 101):
+        result = ""
+        if number % 3 == 0:
+            result += "Fizz"
+        if number % 5 == 0:
+            result += "Buzz"
+        print(result if result else "{} ".format(number), end="")
