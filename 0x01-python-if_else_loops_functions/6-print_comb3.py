c3df6770b11e3ef74c2f82a82a174d8ccd42dd0a
Tunde <tundeogunmodede1@gmail.com>
2024-01-02 16:22:06 -0800

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-for x in range(9):
+for x in range(0, 9):
     for y in range(x + 1 if x > 0 else 1, 10):
-        print('{}{}, '.format(x, y), end='')
-print('89')
+        print('{}{}'.format(x, y), end='')
+        if x != 8 or y != 9:
+            print(', ', end='')
+print()
