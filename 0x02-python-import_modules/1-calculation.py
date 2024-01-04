4b6e387aa554a1b9c27639d9223ae2497062eaa9
Tunde <tundeogunmodede1@gmail.com>
2024-01-04 06:43:57 -0800

    1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
+import calculator_1 #import add, sub, mul, div
 a = 10
 b = 5
-result_addition = add(a, b)
-result_subtraction = sub(a, b)
-result_multiplication = mul(a, b)
-result_division = div(a, b)
+result_addition = calculator_1.add(a, b)
+result_sutraction = calculator_1.sub(a, b)
+result_multiplication = calculator_1.mul(a, b)
+result_division = calculator_1.div(a, b)
 print("{} + {} = {}".format(a, b, result_addition))
-print("{} - {} = {}".format(a, b, result_subtraction))
+print("{} - {} = {}".format(a, b, result_sutraction))
 print("{} * {} = {}".format(a, b, result_multiplication))
 print("{} / {} = {}".format(a, b, result_division))
