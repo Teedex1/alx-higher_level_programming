9020cd49b37891104967c9c4871bbd936589f43e
Tunde <tundeogunmodede1@gmail.com>
2024-01-22 13:22:08 -0800

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -10,12 +10,14 @@ def safe_print_list(my_list=[], x=0):
         The number of elements printed.
     """
 
-    count = 0
-    for i in range(x):
+    index = 0
+
+    while index < x:
         try:
-            print("{}".format(my_list[i]), end="")
-            count += 1
+            print("{}".format(my_list[index]), end="")
         except IndexError:
             break
+        index += 1
+
         print()
-        return count
+        return index
