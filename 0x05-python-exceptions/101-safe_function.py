22890e35d579a35561b5803819cc67e73a29ee96
Tunde <tundeogunmodede1@gmail.com>
2024-01-24 07:19:02 -0800

    be like eno break

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -3,8 +3,9 @@
 from __future__ import print_function
 import sys
 
+
 def safe_function(fct, *args):
-    try: 
+    try:
         count = fct(*args)
     except Exception as e:
         print("Exception: {}".format(e), file=sys.stderr)
