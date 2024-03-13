af50e66de38dd4786a38209e9f06a451530a705d
Tunde <tundeogunmodede1@gmail.com>
2024-03-13 06:22:14 -0700

    new fix

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,6 @@
 -- lists all shows contained in hbtn_0d_tvshows that have atleast one genre linked
 
-SELECT tv_show.title, tv_show_genres.genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 INNER JOIN tv_show_genres
 ON tv_shows.id=tv_show_genres.show_id
